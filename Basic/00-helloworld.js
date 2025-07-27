882e9c56c39865e4a0b6b9b11994cbe2abc0f1cf
Joel Fernandez <ejoelf10@gmail.com>
2025-07-27 13:33:11 +0200

    Inicio Curso

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -1,30 +1,13 @@
-/*
-Clases 1 a 14 (sin código)
-Vídeo: https://youtu.be/1glVfFxj8a4
-
-Clase 15 - Hola mundo
-Vídeo: https://youtu.be/1glVfFxj8a4?t=2390
-*/
-
-// Esto es un comentario simple
+//Esto es un comentario simple
 
 /*
-Esto es
-un comentario
-en varias líneas
+Esto es un comentario
+en varias lineas
 */
 
-console.log("¡Hola, JavaScript!")
-console.log('¡Hola, JavaScript!')
-console.log(`¡Hola, JavaScript!`)
-
-// console.log("¡Hola, JavaScript!")
-
-/*
-console.log("¡Hola, JavaScript!")
-console.log('¡Hola, JavaScript!')
-console.log(`¡Hola, JavaScript!`)
-*/
+console.log("¡Hola, javascript!");
+console.log('¡Hola, javascript!');
+console.log(`¡Hola, javascript!`);
 
 console.log("5")
 console.log(5)
@@ -34,3 +17,7 @@ console.log(5 * 2)
 console.log(5 / 2)
 console.log(5 % 2)
 console.log(5 ** 2)
+
+/* Continuar con el curso en el siguiente link
+https://youtu.be/1glVfFxj8a4?si=MpxBlJcJueskDd22
+*/
